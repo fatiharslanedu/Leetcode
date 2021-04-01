03845cfd6855fe1a585c93fee12e0903da144918
fatiharslanedu <fatiharslanedu@gmail.com>
2021-04-01 17:46:41 +0300

    Added two sum problem.

---
+++ twoSum.py
@@ -0,0 +1,17 @@
+from typing import List 
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    pair = dict()
+    i = 0
+    for n in nums:
+        #*find pairs
+        if n in pair:
+            return [pair[n], i]
+        else:#*Add to dict
+            pair[target - n] = i
+            i += 1
+
+
+nums = [2,7,11,15]
+target = 9
+print(twoSum(nums,target))
