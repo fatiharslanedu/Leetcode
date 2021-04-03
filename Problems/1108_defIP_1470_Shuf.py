fc5f87014e64baa8b81fab912e48b5b9624640bc
fatiharslanedu <fatiharslanedu@gmail.com>
2021-04-04 00:18:28 +0300

    2 problems added.

--- Problems/1108_defIP_1470_Shuf.py
+++ Problems/1108_defIP_1470_Shuf.py
@@ -1,22 +1,27 @@
 from typing import List
 
-
+#todo: 1108. Defanging an IP Address
+#? Easy
 def defangIPaddr(address: str) -> str:
     """'255[.]100[.]50[.]0'"""
-    return address.replace(".","[.]")
+    return address.replace(".", "[.]")
+
 
 address = "255.100.50.0"
-#* print(defangIPaddr(address))
+# * print(defangIPaddr(address))
 
+#todo: 1470. Shuffle the Array
+#? Easy
 def shuffle(nums: List[int], n: int) -> List[int]:
     """Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7]."""
     li = []
     for i in range(0, n):
         li.append(nums[i])
         li.append(nums[n])
-        n += 1    
+        n += 1
     return li
 
-nums = [2,5,1,3,4,7]
+
+nums = [2, 5, 1, 3, 4, 7]
 n = 3
-print(shuffle(nums,n))
+# * print(shuffle(nums,n))
