a20ac646c9839ce7d74cf52b6b1b6b30066fd4cb
fatiharslanedu <fatiharslanedu@gmail.com>
2021-04-04 00:10:45 +0300

    Added 2 more algorithms.

---
+++ Problems/1108_defIP_1470_Shuf.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def defangIPaddr(address: str) -> str:
+    """'255[.]100[.]50[.]0'"""
+    return address.replace(".","[.]")
+
+address = "255.100.50.0"
+#* print(defangIPaddr(address))
+
+def shuffle(nums: List[int], n: int) -> List[int]:
+    """Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7]."""
+    li = []
+    for i in range(0, n):
+        li.append(nums[i])
+        li.append(nums[n])
+        n += 1    
+    return li
+
+nums = [2,5,1,3,4,7]
+n = 3
+print(shuffle(nums,n))
